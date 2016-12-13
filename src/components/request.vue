4b98ae12d0d3e74898309b58d43e442c27657b3f
Neil Chudleigh <nchudleigh@gmail.com>
2016-12-12 23:47:12 -0500

    fixed payload/obj interaction

--- src/components/request.vue
+++ src/components/request.vue
@@ -4,7 +4,7 @@
     <span class="text bold mono s">{{method}}</span>
     <span class="text mono s m1l">{{endpoint}}</span>
     <div class="">
-        <textarea id="textarea" v-model="payload" style="resize:none;height:100px" class="oh text s u-full-width" @keydown="tab" @keyup="resize">
+        <textarea id="textarea" v-model="payload" style="resize:none;height:100px" class="oh text s u-full-width" @keydown.tab.prevent="tab" @keyup="resize">
     </div>
     <button @click="send">Send</button>
     <span class="text mono grey cp hgrey m1l" @click="cancel">Cancel</span>
@@ -28,12 +28,14 @@ export default {
     },
     data() {
         return {
-            error_message: ""
+            error_message: "",
+            payload: {}
         }
     },
     created(){
         setTimeout(this.resize, 1);
         this.bus.$on('edit', ()=>{setTimeout(this.resize, 1)});
+        this.payload = this.obj;
     },
     methods: {
         resize(e) {
@@ -43,17 +45,13 @@ export default {
                 ta.style.height = ta.scrollHeight + 'px';
         },
         tab(e) {
-            var keyCode = e.keyCode || e.which;
-            if (keyCode == 9) {
-                e.preventDefault();
-                var ta = document.getElementById('textarea');
-                var start = ta.selectionStart;
-                var end = ta.selectionEnd;
-                // set textarea value to: text before caret + tab + text after caret
-                ta.value = ta.value.substring(0, start) + "\t" + ta.value.substring(end);
-                // put caret at right position again
-                ta.selectionStart = ta.selectionEnd = start + 1;
-            }
+            var ta = document.getElementById('textarea');
+            var start = ta.selectionStart;
+            var end = ta.selectionEnd;
+            // set textarea value to: text before caret + tab + text after caret
+            ta.value = ta.value.substring(0, start) + "\t" + ta.value.substring(end);
+            // put caret at right position again
+            ta.selectionStart = ta.selectionEnd = start + 1;
         },
         send(){
             console.log();
@@ -69,13 +67,19 @@ export default {
             this.bus.$emit('cancel');
         }
     },
+    watch: {
+        obj: {
+            handler(){
+                // if obj is updated, update payload
+                this.payload = this.obj;
+            },
+            deep:true
+        }
+    },
     computed:{
         endpoint() {
             let id = this.obj.id ? this.obj.id : "";
             return `${api.domain}${this.url}${id}`;
-        },
-        payload() {
-            return this.obj;
         }
     }
 }
